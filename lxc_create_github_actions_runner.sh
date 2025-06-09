c65ecfc5914c44b8dd9467297b6897b992114d4b
stixif <stixif@gmail.com>
2025-06-09 21:10:26 +0200

    Update lxc_create_github_actions_runner.sh

--- lxc_create_github_actions_runner.sh
+++ lxc_create_github_actions_runner.sh
@@ -15,8 +15,8 @@ PCT_CORES="4"
 PCT_MEMORY="4096"
 PCT_SWAP="4096"
 PCT_STORAGE="local-lvm"
-DEFAULT_IP_ADDR="192.168.0.123/24"
-DEFAULT_GATEWAY="192.168.0.1"
+#DEFAULT_IP_ADDR="192.168.0.123/24"
+#DEFAULT_GATEWAY="192.168.0.1"
 
 # Ask for GitHub token and owner/repo if they're not set
 if [ -z "$GITHUB_TOKEN" ]; then
